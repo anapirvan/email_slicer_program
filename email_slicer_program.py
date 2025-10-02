1f7af4011695cc5928dc5c66925a4cd921b7e38f
ana.pirvan <anampirvan05@gmail.com>
2025-10-02 22:11:49 +0300

    second commit

--- email_slicer_program.py
+++ email_slicer_program.py
@@ -1,8 +1,6 @@
 email = input("Enter your email: ")
 
-index = email.index("@")
-
-username = email[:index]
-domain = email[index+1:]
+username = email[:email.index("@")]
+domain = email[email.index("@")+1:]
 
 print(f"Your username is {username} and domain is {domain}")
